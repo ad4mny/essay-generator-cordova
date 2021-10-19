8d6abc9e9daade0d697afab8e3defa53531f3ac1
adamz <58021562+ad4mny@users.noreply.github.com>
2021-10-19 12:00:06 +0800

    added essay title submission

---
+++ www/ajax/vocabulary.js
@@ -0,0 +1,37 @@
+var submitTitle = function () {
+
+    var title = $("#title").val();
+    var email = $("#email").val();
+
+    $.ajax({
+        type: "POST",
+        url: webURL + "api/submit_title",
+        data: {
+            userid: token.id,
+            title: title,
+            email: email
+        },
+        dataType: 'JSON',
+        beforeSend: function () {
+            $('#load_gif').show();
+        },
+        success: function (data) {
+            if (data != false) {
+                location.replace('submission.html');
+            } else {
+                alert('Failed to submit email title, try again later.');
+            }
+        },
+        error: function () {
+            $('#info_box').append('<div class="row">' +
+                '<div class="col">' +
+                '<p class="my-3 text-muted">Internal server error, please reload.</p>' +
+                '</div>' +
+                '</div>');
+        },
+        complete: function () {
+            $('#load_gif').hide();
+        }
+    });
+
+};
